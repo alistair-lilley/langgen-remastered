f8dd7cff1cb42a4f81bca319a2f5ef21a6fac709
alistair-lilley <jklilley12@gmail.com>
2026-01-30 18:20:23 -0700

    IPAContainer written but untested

--- cpp/langgenLib/IPAContainer.cpp
+++ cpp/langgenLib/IPAContainer.cpp
@@ -1,3 +1,206 @@
 /* IPA Container
 This object holds the IPA data for the syllable generation process
 */
+#ifndef LANGGENLIB_IPACONTAINER_CPP
+#define LANGGENLIB_IPACONTAINER_CPP
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "langgenLib/IPAContainer.h"
+
+void getCatnameRangesets(std::string& line, std::string& catname, std::vector<std::string>& rangesets)
+{
+    std::string item;
+    for (auto ch : line)
+    {
+        if (ch == '|')
+        {
+            catname = item;
+            item = "";
+        }
+        else if (ch == ';')
+        {
+            rangesets.push_back(item);
+            item = "";
+        }
+        else
+        {
+            item += ch;
+        }
+    }
+    rangesets.push_back(item);
+}
+
+void getRanges(std::vector<std::string>& rangesets, std::vector<std::vector<std::string>>& ranges)
+{
+    int rangepos = 0;
+    for (auto rangeset : rangesets)
+    {
+        std::string range;
+        for (auto ch : rangeset)
+        {
+            if (ch == ',')
+            {
+                ranges.at(rangepos).push_back(range);
+                range = "";
+            }
+            else
+            {
+                range += ch;
+            }
+        }
+        ranges.at(rangepos).push_back(range);
+        rangepos++;
+    }
+    rangepos = 0;
+}
+
+void expandRanges(std::vector<std::vector<std::string>>& ranges, std::vector<std::vector<int>>& expandedRanges)
+{
+    int rangepos = 0;
+    int start;
+    int end;
+    for (auto rangeset : ranges)
+    {
+        for (auto range : rangeset)
+        {
+            std::string rangenum;
+            for (auto ch : range)
+            {
+                if (ch == ':')
+                {
+                    start = stoi(rangenum);
+                }
+                else
+                {
+                    rangenum += ch;
+                }
+            }
+            end = stoi(rangenum);
+            for (auto ii = start; ii < end; ii++)
+            {
+                expandedRanges.at(rangepos).push_back(ii);
+            }
+        }
+        rangepos++;
+    }
+}
+
+void getLineItems(std::string& line, std::vector<std::string>& lineitems)
+{
+    std::string lineitem;
+    for(auto ch : line)
+    {
+        if (ch == '|')
+        {
+            lineitems.push_back(lineitem);
+            lineitem = "";
+        }
+        else
+        {
+            lineitem += ch;
+        }
+    }
+    lineitems.push_back(lineitem);
+}
+
+void getRow(std::vector<std::string>& lineitems, std::vector<std::vector<int>>& row)
+{
+    std::string pairitem;
+    for (auto lineitem : lineitems)
+    {
+        std::vector<int> tuple;
+        for (auto ch : lineitem)
+        {
+            if (ch == ',')
+            {
+                tuple.push_back(stoi(pairitem));
+                pairitem = "";
+            }
+            else
+            {
+                pairitem += ch;
+            }
+        }
+        row.push_back(tuple);
+    }
+}
+
+IPA::IPA(std::string& DP, std::string& CC, std::string& VC, std::string& CK, std::string& VK)
+{
+    dirpath = DP;
+    cipac = CC;
+    vipac = VC;
+    cipak = CK;
+    vipak = VK;
+}
+
+IPAChart IPA::loadOneChart(std::string& chartname)
+{
+    std::string path = dirpath + "/" + chartname;
+    std::ifstream chartfile;
+
+    try
+    {
+        chartfile.open(path);
+    }
+    catch (const std::ios_base::failure& fail)
+    {
+        std::cout << "Error: file " << path << " not found; " << fail.what() << std::endl;
+    }
+
+    std::string line;
+    IPAChart wholechart;
+
+    while (getline(chartfile, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+        std::vector<std::string> lineitems;
+        getLineItems(line, lineitems);
+        std::vector<std::vector<int>> row;
+        getRow(lineitems, row);
+        wholechart.chart.push_back(row);
+    }    
+    return wholechart;
+}
+
+IPAKey IPA::loadOneKey(std::string& keyname)
+{
+    std::string path = dirpath + "/" + keyname;
+    std::ifstream keyfile;
+
+    try
+    {
+        keyfile.open(path);
+    }
+    catch (const std::ios_base::failure& fail)
+    {
+        std::cout << "Error: file " << path << " not found; " << fail.what() << std::endl;
+    }
+
+    std::string line;
+    IPAKey wholekey;
+
+    while (getline(keyfile, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+        std::string catname;
+        std::vector<std::string> rangesets;
+        getCatnameRangesets(line, catname, rangesets);
+        std::vector<std::vector<std::string>> ranges;
+        getRanges(rangesets, ranges);
+        std::vector<std::vector<int>> expandedRanges;
+        expandRanges(ranges, expandedRanges);
+        wholekey.key.insert({catname, expandedRanges});
+    }
+    return wholekey;
+}
+
+#endif
--- cpp/langgenLib/IPAContainer.h
+++ cpp/langgenLib/IPAContainer.h
@@ -7,20 +7,38 @@ This object holds the IPA data for the syllable generation process
 #include <vector>
 #include <map>
 
+struct IPAChart
+{
+    std::vector<std::vector<std::vector<int>>> chart;
+};
+
+struct IPAKey
+{
+    std::map<std::string, std::vector<std::vector<int>>> key;
+};
+
+// Helpers
+void getCatnameRangesets(std::string&, std::string&, std::string&, std::vector<std::string>&);
+void getRanges(std::vector<std::string>&, std::vector<std::vector<std::string>>&);
+void expandRanges(std::vector<std::vector<std::string>>&, std::vector<std::vector<int>>&);
+void getLineItems(std::string&, std::vector<std::string>&);
+void getRow(std::vector<std::string>&, std::vector<std::vector<int>>&);
+
 class IPA
 {
 public:
-    IPA(std::string, std::string, std::string, std::string, std::string);
+    IPA(std::string&, std::string&, std::string&, std::string&, std::string&);
     void loadIPAChartsKeys();
-    std::vector<int> getFeature(std::string);
-    std::vector<int> removeNotFeature(std::string);
+    std::vector<int> getFeature(std::string&);
+    std::vector<int> removeNotFeature(std::string&);
+    std::string dirpath, cipac, vipac, cipak, vipak;
+    IPAChart loadOneChart(std::string&);
+    IPAKey loadOneKey(std::string&);
 private:
-    std::vector<std::vector<std::vector<int>>> cchart;
-    std::vector<std::vector<std::vector<int>>> vchart;
-    std::map<std::string, std::vector<int>> ckey;
-    std::map<std::string, std::vector<int>> vkey;
-    void loadOneChart(std::string);
-    void loadOneKey(std::string);
+    IPAChart cchart;
+    IPAChart vchart;
+    IPAKey ckey;
+    IPAKey vkey;
 };
 
 #endif
